1de47de0fede88efa45e777cdd33b418bab081a4
portarul23 <blanarugeorge96@gmail.com>
2016-12-12 11:41:30 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-
 window.addEventListener("load", function(e) {
-
+    console.log("simonsacunt");
     var canvas = document.getElementById("mainCanvas");
     var pen = new Pen(canvas);
     console.log(pen.getBackgroundColor());
