1187484f2bcadb4d6c22f3f241354d809fad8cbd
portarul23 <blanarugeorge96@gmail.com>
2016-12-13 17:53:34 +0000

    Update Text.js

--- js/Text.js
+++ js/Text.js
@@ -1,45 +1,19 @@
-function Triangle(x1, y1, x2, y2, lc, fc, lw, f) {
-	this.x1 = x1;
-	this.y1 = y1;
-	this.x2 = x2;
-	this.y2 = y2;
-	this.w = this.x2 - this.x1;
-	this.h = this.y2 - this.y1;
-    this.fillCol = fc;
+function Text(x, y ,text,font,lc) {
+	this.x = x;
+    this.y = y;
+    this.text = text;
+    this.font = font;
     this.lineCol = lc;
-    this.f = f;
-    this.lineWidth = lw;
 }
 
 
 
-Triangle.prototype.draw = function (pen) {
+Text.prototype.draw = function (pen) {
     var ctx = pen.getContext();
-    ctx.lineWidth = this.lineWidth;
-	ctx.beginPath();
-    ctx.strokeStyle = this.lineCol;
-	ctx.moveTo(this.x1 + this.w/2, this.y2);
-	ctx.lineTo(this.x1, this.y2);
-	ctx.lineTo(this.x1+(this.x2-this.x1)/2, this.y1);
-	ctx.lineTo(this.x2,this.y2);
-	ctx.lineTo(this.x1 + this.w/2, this.y2);	
-	ctx.stroke();
-	ctx.closePath();
+    ctx.beginPath();
+    ctx.font = this.font;
+    ctx.fillStyle = this.lineCol;
+    ctx.fillText(this.text, this.x, this.y);
+    ctx.closePath();
 }
 
-Triangle.prototype.fill = function (pen) {
-    var ctx = pen.getContext();
-    ctx.lineWidth = this.lineWidth;
-	ctx.beginPath();
-    ctx.strokeStyle = this.lineCol;
-    ctx.fillStyle = this.fillCol;
-	ctx.moveTo(this.x1 + this.w/2, this.y2);
-	ctx.lineTo(this.x1, this.y2);
-	ctx.lineTo(this.x1+(this.x2-this.x1)/2, this.y1);
-	ctx.lineTo(this.x2,this.y2);
-	ctx.lineTo(this.x1 + this.w/2, this.y2);
-
-	ctx.stroke();
-	ctx.fill();
-	ctx.closePath();
-}
