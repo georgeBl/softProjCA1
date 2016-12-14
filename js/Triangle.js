f31034fc4f1a4f0c5f2ded363f19d05839aa526a
portarul23 <blanarugeorge96@gmail.com>
2016-12-14 22:21:09 +0000

    Update Triangle.js

--- js/Triangle.js
+++ js/Triangle.js
@@ -1,4 +1,4 @@
-function Triangle(x1, y1, x2, y2, lc, fc, lw, f) {
+function Triangle(x1, y1, x2, y2, lc, fc, lw, f, g, gradCol1, gradCol2, s) {
 	this.x1 = x1;
 	this.y1 = y1;
 	this.x2 = x2;
@@ -9,6 +9,12 @@ function Triangle(x1, y1, x2, y2, lc, fc, lw, f) {
     this.lineCol = lc;
     this.f = f;
     this.lineWidth = lw;
+    this.grad = g;
+    this.x3 = this.x+this.width;
+    this.y3 = this.y+this.height;
+    this.gradCol1 = gradCol1;
+    this.gradCol2 = gradCol2;
+    this.stroke = s;
 }
 
 
@@ -30,16 +36,62 @@ Triangle.prototype.draw = function (pen) {
 Triangle.prototype.fill = function (pen) {
     var ctx = pen.getContext();
     ctx.lineWidth = this.lineWidth;
-	ctx.beginPath();
     ctx.strokeStyle = this.lineCol;
-    ctx.fillStyle = this.fillCol;
+    var centerX = this.x + this.width / 2; // This is finding the point at which x is at the center of the shape.
+    var centerY = this.y + this.height / 2; // This is finding the point at which y is at the center of the shape.
+    var rad1A = Math.abs(this.width/4); // This is finding the absolute value of the width variable.
+    var rad1B = Math.abs(this.width/2);
+    var rad2A = Math.abs(this.height/4); // This is finding the absolute value of the height variable.
+    var rad2B = Math.abs(this.height/2);
+    console.log("centerX: " + centerX);
+    console.log("centerY: " + centerY);
+    console.log("rad1A: " + rad1A);
+    console.log("rad1B: " + rad1B);
+    console.log("rad2A: " + rad2A);
+    console.log("rad2B: " + rad2B);
+    if (this.grad === "linear") {
+        var linGrad = ctx.createLinearGradient(this.x, this.y, this.x3, this.y3);
+        linGrad.addColorStop(0, this.gradCol1);
+        linGrad.addColorStop(1, this.gradCol2);
+        pen.setFillColor(linGrad);
+    }
+    else if (this.grad === "radial") {
+        if (Math.abs(this.width) > Math.abs(this.height)) {
+            var radGrad = ctx.createRadialGradient(centerX, centerY, rad1A, centerX, centerY, rad1B);
+        }
+        else {
+            var radGrad = ctx.createRadialGradient(centerX, centerY, rad2A, centerX, centerY, rad2B);
+        }
+        radGrad.addColorStop(0, this.gradCol1);
+        radGrad.addColorStop(1, this.gradCol2);
+        pen.setFillColor(radGrad);
+    }
+    else if(this.grad === "pattern1"){
+        var img = new Image();
+        img.src ="img/pattern1.png";
+        var pat=ctx.createPattern(img,"repeat");
+        pen.setFillColor(pat);
+    }
+    else if(this.grad === "pattern2"){
+        var img = new Image(); 
+        img.src ="img/pattern2.jpg";
+        var pat=ctx.createPattern(img,"repeat");
+        pen.setFillColor(pat);
+    }
+    else {
+        pen.setFillColor(this.fillCol);
+        ctx.fillStyle = this.fillCol;
+    }
+    ctx.beginPath();
 	ctx.moveTo(this.x1 + this.w/2, this.y2);
 	ctx.lineTo(this.x1, this.y2);
 	ctx.lineTo(this.x1+(this.x2-this.x1)/2, this.y1);
 	ctx.lineTo(this.x2,this.y2);
 	ctx.lineTo(this.x1 + this.w/2, this.y2);
-
-	ctx.stroke();
+    if(this.stroke){
+        ctx.stroke();    
+    }
+	
 	ctx.fill();
 	ctx.closePath();
 }
