f39da87574a41655472070982987b10c3a7fe33f
portarul23 <blanarugeorge96@gmail.com>
2016-12-14 22:20:18 +0000

    Update Rectangle.js

--- js/Rectangle.js
+++ js/Rectangle.js
@@ -1,4 +1,4 @@
-function Rectangle(x, y, w, h, lc, fc, lw, fi, g) {
+function Rectangle(x, y, w, h, lc, fc, lw, fi, g, gradCol1, gradCol2, s) {
 //	this.x = x;
 //	this.y = y;
 //	this.width = w;
@@ -23,9 +23,13 @@ function Rectangle(x, y, w, h, lc, fc, lw, fi, g) {
 	this.fillCol = fc;
 	this.f = fi;
     this.lineWidth = lw;
+    
 	this.grad = g;
     this.x2 = this.x+this.width;
     this.y2 = this.y+this.height;
+    this.gradCol1 = gradCol1;
+    this.gradCol2 = gradCol2;
+    this.stroke = s;
 }
 
 Rectangle.prototype.draw = function (pen) {
@@ -48,13 +52,10 @@ Rectangle.prototype.fill = function (pen) {
     var rad1B = Math.abs(this.width/2);
     var rad2A = Math.abs(this.height/4); // This is finding the absolute value of the height variable.
     var rad2B = Math.abs(this.height/2);
-    
-    var ctx = pen.getContext();
-    
     if (this.grad === "linear") {
         var linGrad = ctx.createLinearGradient(this.x, this.y, this.x2, this.y2);
-        linGrad.addColorStop(0, "red");
-        linGrad.addColorStop(1, "white");
+        linGrad.addColorStop(0, this.gradCol1);
+        linGrad.addColorStop(1, this.gradCol2);
         pen.setFillColor(linGrad);
     }
     else if (this.grad === "radial") {
@@ -64,41 +65,32 @@ Rectangle.prototype.fill = function (pen) {
         else {
             var radGrad = ctx.createRadialGradient(centerX, centerY, rad2A, centerX, centerY, rad2B);
         }
-        radGrad.addColorStop(0, "red");
-        radGrad.addColorStop(1, "white");
+        radGrad.addColorStop(0, this.gradCol1);
+        radGrad.addColorStop(1, this.gradCol2);
         pen.setFillColor(radGrad);
     }
-    else {      
+    else if(this.grad === "pattern1"){
+        var img = new Image();
+        img.src ="img/pattern1.png";
+        var pat=ctx.createPattern(img,"repeat");
+        pen.setFillColor(pat);
+    }
+    else if(this.grad === "pattern2"){
+        var img = new Image(); 
+        img.src ="img/pattern2.jpg";
+        var pat=ctx.createPattern(img,"repeat");
+        pen.setFillColor(pat);
+    }
+    else {
+        pen.setFillColor(this.fillCol);
         ctx.fillStyle = this.fillCol;
     }
     ctx.beginPath();
-    ctx.fillRect(this.x, this.y, this.width, this.height);
-    ctx.stroke();
+    ctx.rect(this.x, this.y, this.width, this.height);
+    if(this.stroke){
+        ctx.stroke();
+    }
     ctx.fill();
     ctx.closePath();   
     
 }
-    
-//    if(this.grad === "linear"){
-//        ctx.beginPath();
-//        var grd = ctx.createLinearGradient(0,0,0,Math.abs(this.x+this.width));
-//        grd.addColorStop(0,"black");
-//        grd.addColorStop(1,"red");
-//        ctx.fillStyle = grd;
-//        ctx.rect(this.x, this.y, this.width, this.height);
-//        ctx.stroke();
-//        ctx.fill();
-//        ctx.closePath();
-//    }
-//    
-//    else if (this.grad === "radial") {
-//        ctx.beginPath();
-//        var grd=ctx.createRadialGradient(this.width/2+this.x,this.height/2,Math.abs(this.width/2),this.width/2,this.height/2,Math.abs(this.y));
-//        grd.addColorStop(0,"red");
-//        grd.addColorStop(1,"white");
-//        ctx.fillStyle= grd;
-//        ctx.rect(this.x, this.y, this.width, this.height);
-//        ctx.stroke();
-//        ctx.fill();
-//        ctx.closePath();
-//        console.log("amIstillHere?");
